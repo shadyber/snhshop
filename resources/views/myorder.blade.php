424be7a4ccab90e2f37cd9c9669a6e056da73d88
shadyber <shady.ber9@gmail.com>
2021-08-31 08:05:05 +0300

    myorderupdate
    myorderupdate

--- resources/views/myorder.blade.php
+++ resources/views/myorder.blade.php
@@ -31,18 +31,22 @@
         {{dd($order)}}
         <div class="card" style="width: 18rem;">
             <img src="#" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+            <div class="card-body {{$order->status == 'sent' ? 'bg-info' : 'bg-succuss'}}">
+                <h5 class="card-title">Order {{$order->created_at}}</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
+
+                @foreach($cart_item=json_decode($order->cart) as $cart)
+
+                    <li class="btn btn-outline-primary pull-right">
+                        <a href="/item/{{$cart->slug}}" class="list-inline-item">{{$cart->name}}</a>  X {{$cart->quantity}}
+                    </li>
+                @endforeach
+
             </ul>
             <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+
             </div>
         </div>
 
